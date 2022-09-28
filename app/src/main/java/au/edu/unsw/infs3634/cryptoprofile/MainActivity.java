e1ed13f10812213165c2fde32e71dc405a153106
BettyLi103 <z5271723@ad.unsw.edu.au>
2022-09-29 00:56:37 +1000

    Week03 preparation complete

--- app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import au.edu.unsw.infs3634.cryptoprofile.api.Coin;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -15,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("INFS3634 CryptoProfile");
+        Log.d(TAG,"MainActivity launched");
         btnLaunchActivity = findViewById(R.id.btnLaunch);
+        btnLaunchActivity.setText("Launch Detail Activity");
         btnLaunchActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     // Called when the user taps launch button
     public void launchDetailActivity(String msg) {
         Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        Coin coin = Coin.getCoins().get(0);
         intent.putExtra(DetailActivity.INTENT_MESSAGE ,msg);
         startActivity(intent);
     }
